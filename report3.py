4047032031d94087686bc205419a35889c936b70
N1 <pathaknishant003@gmail.com>
2023-08-28 11:26:02 -0400

    adding

---
+++ report3.py
@@ -0,0 +1,7 @@
+As of my last update in September 2021, the salary for a data engineer in India with two years of experience can vary based on the company, location, and other factors. However, I can provide you with a rough estimate.
+
+On average, a data engineer with two years of experience in India can earn anywhere between ₹5,00,000 to ₹10,00,000 per annum. This translates to a monthly salary of approximately ₹41,666 to ₹83,333.
+
+Keep in mind that these figures are only estimates and may have changed since my last update. Salaries can vary significantly based on the specific company, the candidate's skills and qualifications, the city or region they are located in, and the demand for data engineering professionals in the market.
+
+For the most up-to-date and accurate information on data engineer salaries in India, I recommend consulting online job portals, salary survey websites, or reaching out to recruitment agencies specialized in technology and data-related roles.
